fd467a8c549250613b4481622c66ba4e0c4bfe9b
Evgeny <jenkneo.lal@gmail.com>
2024-04-17 23:12:37 +0400

    add router

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,43 @@
-import sun from "./../../img/icons/sun.svg"
-import moon from "./../../img/icons/moon.svg"
+import sun from "./../../img/icons/sun.svg";
+import moon from "./../../img/icons/moon.svg";
+
+import { NavLink } from "react-router-dom";
 
 import "./style.css"
 
 function Navbar () {
   return (
     <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>React</strong> portfolio</a>
+      <div className="container">
+        <div className="nav-row">
+          <NavLink to="/" className="logo">
+            <strong>React</strong> portfolio
+          </NavLink>
 
-                <button className="dark-mode-btn">
-                    <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                    <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                </button>
+          <button className="dark-mode-btn">
+            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+          </button>
 
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
+          <ul className="nav-list">
+            <li className="nav-list__item">
+              <NavLink to="/" className="nav-list__link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink to="/projects" className="nav-list__link">
+              Projects
+              </NavLink>
+            </li>
+            <li className="nav-list__item">
+              <NavLink to="/contacts" className="nav-list__link">
+              Contacts
+              </NavLink>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
   );
 }
